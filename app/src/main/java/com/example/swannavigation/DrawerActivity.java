66c68e9a584ff3104d3be0769eca29dee1e2f45d
Ciaran Swanepoel <ciaranswan@gmail.com>
2020-05-16 15:41:53 +0200

    MapBox Navigation working in Drawer activity, UI should be modified as well as UX. removed unnesessary code from main and drawer activity

--- app/src/main/java/com/example/swannavigation/DrawerActivity.java
+++ app/src/main/java/com/example/swannavigation/DrawerActivity.java
@@ -1,55 +1,21 @@
 package com.example.swannavigation;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.gson.JsonObject;
-import com.mapbox.android.core.permissions.PermissionsListener;
-import com.mapbox.android.core.permissions.PermissionsManager;
-import com.mapbox.api.geocoding.v5.models.CarmenFeature;
-import com.mapbox.geojson.Point;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.location.LocationComponent;
-import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
-import com.mapbox.mapboxsdk.location.modes.CameraMode;
-import com.mapbox.mapboxsdk.location.modes.RenderMode;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.maps.SupportMapFragment;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
 
-import java.util.List;
-
-public class DrawerActivity extends AppCompatActivity implements PermissionsListener {
+public class DrawerActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
-    private CarmenFeature home;
-    private CarmenFeature work;
-    private MapboxMap mapboxMap;
-    private PermissionsManager permissionsManager;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +24,6 @@ public class DrawerActivity extends AppCompatActivity implements PermissionsList
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Mapbox.getInstance(this, getString(R.string.access_token));
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -71,99 +36,7 @@ public class DrawerActivity extends AppCompatActivity implements PermissionsList
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        addUserLocations();
-        initSearchFab();
 
-        // Create supportMapFragment
-        SupportMapFragment mapFragment;
-        if (savedInstanceState == null) {
-
-// Create fragment
-            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-// Build a Mapbox map
-            MapboxMapOptions options = MapboxMapOptions.createFromAttributes(this, null);
-            options.camera(new CameraPosition.Builder()
-                    .target(new LatLng(38.899895, -77.03401))
-                    .zoom(16)
-                    .build());
-
-// Create map fragment
-            mapFragment = SupportMapFragment.newInstance(options);
-
-// Add map fragment to parent container
-            transaction.add(R.id.location_frag_container, mapFragment, "com.mapbox.map");
-            transaction.commit();
-        } else {
-            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("com.mapbox.map");
-        }
-
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                    DrawerActivity.this.mapboxMap = mapboxMap;
-                    mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded() {
-                        @Override
-                        public void onStyleLoaded(@NonNull Style style) {
-                            enableLocationComponent(style);
-                        }
-                    });
-                }
-            });
-        }
-
-    }
-
-    @SuppressWarnings({"MissingPermission"})
-    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-// Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-
-// Get an instance of the LocationComponent.
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
-
-// Activate the LocationComponent
-            locationComponent.activateLocationComponent(
-                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
-
-// Enable the LocationComponent so that it's actually visible on the map
-            locationComponent.setLocationComponentEnabled(true);
-
-// Set the LocationComponent's camera mode
-            locationComponent.setCameraMode(CameraMode.TRACKING);
-
-// Set the LocationComponent's render mode
-            locationComponent.setRenderMode(RenderMode.NORMAL);
-        } else {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onPermissionResult(boolean granted) {
-        if (granted) {
-            mapboxMap.getStyle(new Style.OnStyleLoaded() {
-                @Override
-                public void onStyleLoaded(@NonNull Style style) {
-                    enableLocationComponent(style);
-                }
-            });
-        } else {
-            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
-            finish();
-        }
     }
 
     @Override
@@ -180,37 +53,5 @@ public class DrawerActivity extends AppCompatActivity implements PermissionsList
                 || super.onSupportNavigateUp();
     }
 
-    private void initSearchFab() {
-        findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new PlaceAutocomplete.IntentBuilder()
-                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
-                        .placeOptions(PlaceOptions.builder()
-                                .backgroundColor(Color.parseColor("#EEEEEE"))
-                                .limit(10)
-                                .addInjectedFeature(home)
-                                .addInjectedFeature(work)
-                                .build(PlaceOptions.MODE_CARDS))
-                        .build(DrawerActivity.this);
-                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
-            }
-        });
-    }
 
-    private void addUserLocations() {
-        home = CarmenFeature.builder().text("Mapbox SF Office")
-                .geometry(Point.fromLngLat(-122.3964485, 37.7912561))
-                .placeName("50 Beale St, San Francisco, CA")
-                .id("mapbox-sf")
-                .properties(new JsonObject())
-                .build();
-
-        work = CarmenFeature.builder().text("Mapbox DC Office")
-                .placeName("740 15th Street NW, Washington DC")
-                .geometry(Point.fromLngLat(-77.0338348, 38.899750))
-                .id("mapbox-dc")
-                .properties(new JsonObject())
-                .build();
-    }
 }
--- app/src/main/java/com/example/swannavigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/swannavigation/ui/home/HomeFragment.java
@@ -1,5 +1,14 @@
 package com.example.swannavigation.ui.home;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,82 +21,91 @@ import androidx.fragment.app.Fragment;
 
 import com.example.swannavigation.R;
 import com.google.gson.JsonObject;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
+
+import static android.os.Looper.getMainLooper;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements
+        OnMapReadyCallback, PermissionsListener, Callback<DirectionsResponse> {
+
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
     private CarmenFeature home;
     private CarmenFeature work;
     private MapView mapView;
     private String geojsonSourceLayerId = "geojsonSourceLayerId";
     private String symbolIconId = "symbolIconId";
+    public static Location currentLocation;
+    LocationManager locationManager;
+    String provider;
+    private MapboxMap mapBoxMap;
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000;
+    private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
+    private NavigationMapRoute navigationMapRoute;
+    private HomeFragmentLocationCallback callback = new HomeFragmentLocationCallback(this);
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Mapbox.getInstance(this.getContext(), getString(R.string.access_token));
+        Mapbox.getInstance(this.requireContext(), getString(R.string.access_token));
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        provider = locationManager.getBestProvider(new Criteria(), false);
 
 
         final View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         mapView = rootView.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-
-
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-
-                        addUserLocations();
-
-                        // Add the symbol layer icon to map for future use
-                        //   style.addImage(symbolIconId, BitmapFactory.decodeResource(
-                        //       DrawerActivity.this.getResources(), R.drawable.blue_marker_view));
-
-
-                        //  enableLocationComponent(style);
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-
-                        //  navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxmap);
-
-
-// Create an empty GeoJSON source using the empty feature collection
-                        setUpSource(style);
-
-// Set up a new symbol layer for displaying the searched location's feature coordinates
-                        setupLayer(style);
-                    }
-                });
-                // Set the camera's starting position
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(new LatLng(41.885, -87.679)) // set the camera's center position
-                        .zoom(12)  // set the camera's zoom level
-                        .tilt(20)  // set the camera's tilt
-                        .build();
-
-                // Move the camera to that position
-                mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                Toast.makeText(rootView.getContext(), "TEST", Toast.LENGTH_SHORT).show();
-            }
-
-        });
+        mapView.getMapAsync(this);
         return rootView;
+
+
     }
 
 
@@ -161,5 +179,265 @@ public class HomeFragment extends Fragment {
         mapView.onSaveInstanceState(outState);
     }
 
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this.getContext())) {
 
+// Get an instance of the LocationComponent.
+            LocationComponent locationComponent = mapBoxMap.getLocationComponent();
+
+// Activate the LocationComponent
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this.getContext(), loadedMapStyle).build());
+
+// Enable the LocationComponent so that it's actually visible on the map
+            locationComponent.setLocationComponentEnabled(true);
+
+// Set the LocationComponent's camera mode
+
+            locationComponent.setCameraMode(CameraMode.TRACKING,
+                    750L /*duration*/,
+                    16.0 /*zoom*/,
+                    null /*bearing, use current/determine based on the tracking mode*/,
+                    10.0 /*tilt*/,
+                    null /*transition listener*/);
+
+// Set the LocationComponent's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+            initLocationEngine();
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this.getActivity());
+        }
+    }
+
+    /**
+     * Set up the LocationEngine and the parameters for querying the device's location
+     */
+    @SuppressLint("MissingPermission")
+    private void initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this.requireContext());
+
+        LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+
+        locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+        locationEngine.getLastLocation(callback);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapBoxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this.requireContext(), R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private void initSearchFab() {
+        mapView.findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+                                .addInjectedFeature(home)
+                                .addInjectedFeature(work)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(HomeFragment.this.getActivity());
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        mapBoxMap = mapboxMap;
+
+        mapBoxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+
+                initSearchFab();
+
+                addUserLocations();
+
+                // Add the symbol layer icon to map for future use
+                style.addImage(symbolIconId, BitmapFactory.decodeResource(
+                        HomeFragment.this.getResources(), R.drawable.blue_marker_view));
+
+                Toast.makeText(HomeFragment.this.getActivity(), "Style Loaded", Toast.LENGTH_LONG);
+                enableLocationComponent(style);
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+                navigationMapRoute = new NavigationMapRoute(null, mapView, mapBoxMap);
+
+
+// Create an empty GeoJSON source using the empty feature collection
+                setUpSource(style);
+
+// Set up a new symbol layer for displaying the searched location's feature coordinates
+                setupLayer(style);
+            }
+        });
+    }
+
+    @Override
+    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+        if (response.isSuccessful()
+                && response.body() != null
+                && !response.body().routes().isEmpty()) {
+            List<DirectionsRoute> routes = response.body().routes();
+            navigationMapRoute.addRoutes(routes);
+            //routeLoading.setVisibility(View.INVISIBLE);
+            //fabRemoveRoute.setVisibility(View.VISIBLE);
+
+            // boolean simulateRoute = true;
+
+// Create a NavigationLauncherOptions object to package everything together
+            NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                    .directionsRoute(response.body().routes().get(0))
+                    .shouldSimulateRoute(true)
+                    .build();
+
+// Call this method with Context from within an Activity
+            NavigationLauncher.startNavigation(this.requireActivity(), options);
+        }
+    }
+
+    @Override
+    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+
+            // Retrieve selected location's CarmenFeature
+            CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
+
+            // Create a new FeatureCollection and add a new Feature to it using selectedCarmenFeature above.
+            // Then retrieve and update the source designated for showing a selected location's symbol layer icon
+
+            if (mapBoxMap != null) {
+                Style style = mapBoxMap.getStyle();
+                if (style != null) {
+                    GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
+                    if (source != null) {
+                        source.setGeoJson(FeatureCollection.fromFeatures(
+                                new Feature[]{Feature.fromJson(selectedCarmenFeature.toJson())}));
+                    }
+
+                    // Move map camera to the selected location
+                    Point destination;
+
+
+                    destination = Point.fromLngLat(((Point) selectedCarmenFeature.geometry()).longitude(),
+                            ((Point) selectedCarmenFeature.geometry()).latitude());
+
+
+                    BuildRoute(destination);
+
+                    /**
+                     mapBoxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+
+
+                     new CameraPosition.Builder()
+                     .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
+                     ((Point) selectedCarmenFeature.geometry()).longitude()))
+                     .zoom(14)
+                     .build()), 4000);
+                     **/
+                }
+            }
+        }
+    }
+
+    protected void BuildRoute(Point destination) {
+        Point origin = Point.fromLngLat(currentLocation.getLongitude(), currentLocation.getLatitude());
+        //Point destination = Point.fromLngLat(-77.0365, 38.8977);
+
+        NavigationRoute.builder(this.getContext())
+                .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
+                .origin(origin)
+                .voiceUnits(DirectionsCriteria.IMPERIAL)
+                .destination(destination)
+                .build()
+                .getRoute(this);
+    }
+
+
+    public static class HomeFragmentLocationCallback
+            implements LocationEngineCallback<LocationEngineResult> {
+
+        private final WeakReference<HomeFragment> activityWeakReference;
+
+        public HomeFragmentLocationCallback(HomeFragment activity) {
+            this.activityWeakReference = new WeakReference<>(activity);
+        }
+
+        /**
+         * The LocationEngineCallback interface's method which fires when the device's location has changed.
+         *
+         * @param result the LocationEngineResult object which has the last known location within it.
+         */
+        @SuppressLint("StringFormatInvalid")
+        @Override
+        public void onSuccess(LocationEngineResult result) {
+            HomeFragment activity = activityWeakReference.get();
+
+            if (activity != null) {
+                currentLocation = result.getLastLocation();
+
+
+                if (currentLocation == null) {
+                    return;
+                }
+
+                // Create a Toast which displays the new location's coordinates
+                Toast.makeText(activity.getActivity(), String.format(activity.getString(R.string.new_location),
+                        String.valueOf(currentLocation.getLatitude()), String.valueOf(currentLocation.getLongitude())),
+                        Toast.LENGTH_SHORT).show();
+
+                // Pass the new location to the Maps SDK's LocationComponent
+                if (activity.mapBoxMap != null && result.getLastLocation() != null) {
+                    activity.mapBoxMap.getLocationComponent().forceLocationUpdate(result.getLastLocation());
+                }
+            }
+        }
+
+        /**
+         * The LocationEngineCallback interface's method which fires when the device's location can not be captured
+         *
+         * @param exception the exception message
+         */
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+            Timber.d(exception.getLocalizedMessage());
+            HomeFragment activity = activityWeakReference.get();
+            if (activity != null) {
+                Toast.makeText(activity.getActivity(), exception.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
